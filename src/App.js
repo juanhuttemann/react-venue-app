e20271a2c370d36bb44370779f9e9077acb96c84
Juan Hüttemann <juanfhuttemann@gmail.com>
2019-06-04 14:26:48 -0400

    add several components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from './components/Header'
 import './resources/styles.css'
+
 import Featured from './components/featured'
+import VenueInfo from './components/venueInfo'
+import Highlight from './components/highlights'
 
 const App = ()=> {
   return (
     <div className="App" style={{ height:"1500px", background: 'cornflowerblue'}}>
       <Header/>
       <Featured/>
+      <VenueInfo/>
+      <Highlight/>
     </div>
   );
 }
