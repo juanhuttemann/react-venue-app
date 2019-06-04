c0bc51c8fdc3bca83c3e905feda9c3138fc364bd
Juan Hüttemann <juanfhuttemann@gmail.com>
2019-06-04 14:44:17 -0400

    Add discount percentage animation

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -3,23 +3,55 @@ import Fade from 'react-reveal/Fade'
 import Slide from 'react-reveal/Slide'
 
 export default class Discount extends Component {
+
+    state = {
+        discountStart:0,
+        discountEnd: 30,
+    }
+
+    percentage = ()=> {
+        if(this.state.discountStart < this.state.discountEnd){
+            this.setState({
+                discountStart: this.state.discountStart + 1
+            })
+        }
+    }
+
+    componentDidUpdate(){
+        setTimeout(()=>{
+            this.percentage()
+        }, 30)
+    }
+
     render() {
         return (
             <div className="center_wrapper">
                 <div className="discount_wrapper">
-                    <div className="discount_porcentage">
-                        <span>23 %</span>
-                        <span>OFF</span>
-                    </div>
-                    <div className="discount_description">
-                        <h3>Purchase Tickets Before 20th JUNE</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Reiciendis incidunt eos, repellat nemo aut amet, 
-                            dolore itaque ab quasi temporibus sed dolorum expedita tempore assumenda praesentium nisi, 
-                            corporis eum nulla!
-                        </p>
-                    </div>
+                    <Fade 
+                        onReveal={()=>this.percentage()}
+                    >
+                        <div className="discount_porcentage">
+                            <span>{this.state.discountStart}%</span>
+                            <span>OFF</span>
+                        </div>
+                    </Fade>
+                    <Slide right>
+                        <div className="discount_description">
+                            <h3>Purchase Tickets Before 20th JUNE</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Reiciendis incidunt eos, repellat nemo aut amet, 
+                                dolore itaque ab quasi temporibus sed dolorum expedita tempore assumenda praesentium nisi, 
+                                corporis eum nulla!
+                            </p>
+                            <div>
+                                button
+                            </div>
+                            <div>
+
+                            </div>
+                        </div>
+                    </Slide>
                 </div>
             </div>
         )
