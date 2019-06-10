f6b89700274f6f4401c2b5ed38900238dd27847e
Juan Hüttemann <juanfhuttemann@gmail.com>
2019-06-10 10:48:57 -0400

    Updates

---
+++ precache-manifest.19c12e234436c11144f40e69ad162831.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5cadab0e12e67685829792ea65e7b4",
+    "url": "/react-venue/index.html"
+  },
+  {
+    "revision": "83245426ad2b0bea21f1",
+    "url": "/react-venue/static/css/main.d6fa7445.chunk.css"
+  },
+  {
+    "revision": "b1feac3b51f8bccce94b",
+    "url": "/react-venue/static/js/2.148859d7.chunk.js"
+  },
+  {
+    "revision": "83245426ad2b0bea21f1",
+    "url": "/react-venue/static/js/main.d1010890.chunk.js"
+  },
+  {
+    "revision": "052f17bd0caeb780e48e",
+    "url": "/react-venue/static/js/runtime~main.739eada5.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/react-venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/react-venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/react-venue/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
